9421ad075bdcb87bfcbf87ad22be49552f3f34c2
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-17 18:48:18 -0500

    gitflow-feature-stash: add-components-to-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
-import { CTA, Brand, Navbad } from './components'
+import { CTA, Brand, Navbar } from './components'
 
 const App = () => {
     return (
         <div className="App">
             <div className="gradient__bg">
                 <Navbar />
+                <Header />
             </div>
         </div>
     );
