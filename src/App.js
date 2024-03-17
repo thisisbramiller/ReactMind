3c7fddd477c61cd474f8f2c54550931b87d24aca
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-17 18:44:22 -0500

    feat: :lipstick: add navbar and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbad } from './components'
+
 const App = () => {
     return (
-        <div>
-            <h1>React Mind</h1>
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+            </div>
         </div>
     );
 }
