63e79f9a5b77ce905d228a437b4db22c1ad14fd6
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-17 22:19:54 -0500

    feat: :lipstick: add links to navbar mobile menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -28,6 +28,17 @@ const Navbar = () => {
                     ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
                     : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
                     }
+                    {toggleMenu && (
+                        <div className="gpt3__navbar-menu_container scale-up-center">
+                            <div className="gpt3__navbar-menu_container-links" >
+                                <p><a href="#home">Home</a></p>
+                                <p><a href="#wgpt3">What is GPT3?</a></p>
+                                <p><a href="#possibility">Open AI</a></p>
+                                <p><a href="#features">Case Studies</a></p>
+                                <p><a href="#blog">Library</a></p>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
