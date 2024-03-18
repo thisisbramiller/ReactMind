c8b15d606e2dd3311b61a78b1bfa09cdb00fc1cd
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-17 21:22:11 -0500

    feat: :lipstick: add basic structure for Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
-        <h1>Navbar</h1>
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt="logo" />
+                </div>
+            </div>
         </div>
     );
 }
