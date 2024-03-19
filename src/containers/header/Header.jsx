0bd8fa4fa63cb6b9b72da8ab56913cc54b148bd0
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-19 10:00:11 -0500

    feat: :lipstick: add Header component with email input and Get Started button

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,6 +6,10 @@ const Header = () => {
         <div className="gpt__header section__padding" id="home">
             <div className="gpt3__header-content">
                 <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
+                <div className="gpt3__header-content__input">
+                    <input type="email" placeholder='Enter your email' />
+                    <button type="button">Get Started</button>
+                </div>
             </div>
         </div>
     );
