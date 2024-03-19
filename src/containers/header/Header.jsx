75ac49f84bca6026868f90b7ebd2bcaa7f228082
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-19 09:57:03 -0500

    feat: :lipstick: create Header component with gradient text

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,8 +3,10 @@ import './header.css'
 
 const Header = () => {
     return (
-        <div>
-        <h1>Header</h1>
+        <div className="gpt__header section__padding" id="home">
+            <div className="gpt3__header-content">
+                <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
+            </div>
         </div>
     );
 }
