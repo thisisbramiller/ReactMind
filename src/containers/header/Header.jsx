6774b64d0acf401f2323851a6ac7c5708cc30aa8
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-19 10:11:46 -0500

    feat: :lipstick: Add people image and access request count to header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './header.css'
+import people from '../../assets/people.png'
 
 const Header = () => {
     return (
@@ -10,6 +11,10 @@ const Header = () => {
                     <input type="email" placeholder='Enter your email' />
                     <p>Join our community today and gain access to a wealth of resources in the field of AI. Stay updated with the latest advancements, participate in engaging discussions, and collaborate with like-minded individuals. Don't miss out on this opportunity to be a part of our dynamic and innovative community.</p>
                     <button type="button">Get Started</button>
+                </div>
+                <div className="gpt3__header-content__people">
+                    <img src={people} alt="people" />
+                    <p>2,048 people requested access in the last 24 hours</p>
                 </div>
             </div>
         </div>
