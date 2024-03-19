edd437cc294413bedabd0473fdafce08f51c0f4b
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-19 11:46:02 -0500

    fix: :pencil2: :bug: fix header class name

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,7 +5,7 @@ import ai from '../../assets/ai.png'
 
 const Header = () => {
     return (
-        <div className="gpt__header section__padding" id="home">
+        <div className="gpt3__header section__padding" id="home">
             <div className="gpt3__header-content">
                 <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
                 <p>Join our community today and gain access to a wealth of resources in the field of AI. Stay updated with the latest advancements, participate in engaging discussions, and collaborate with like-minded individuals. Don't miss out on this opportunity to be a part of our dynamic and innovative community.</p>
