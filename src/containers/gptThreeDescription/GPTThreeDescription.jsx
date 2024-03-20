85ebd646bd47303585660c583da11a847532ea10
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-20 11:02:45 -0500

    feat: :lipstick: add GPTThreeDescription component
    This commit introduces the GPTThreeDescription component. This component includes a heading and a container with multiple Feature components. It's styled with the 'gpt3__gpt3Description' class and related sub-classes.

--- src/containers/gptThreeDescription/GPTThreeDescription.jsx
+++ src/containers/gptThreeDescription/GPTThreeDescription.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './gptThreeDescription.css';
+import { Feature } from '../../components';
 
 const GPTThreeDescription = () => {
     return (
-        <div>
-        <h1>GPTThreeDescription</h1>
+        <div className="gpt3__gpt3Description section_margin" id="gpt3desc">
+            <div className="gpt3__gpt3Description-feature">
+                <Feature />
+            </div>
+            <div className="gpt3__gpt3Description-heading">
+                <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+                <p>Explore The Library</p>
+            </div>
+            <div className="gpt3__gpt3Description-container">
+                <Feature />
+                <Feature />
+                <Feature /> 
+            </div>
         </div>
     );
 }
