803182bd4d6020d98deef97f5dd13777eb7b3fc3
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-19 21:01:27 -0500

    refactor: :recycle: sort brands by popularity

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -6,19 +6,19 @@ const Brand = () => {
     return (
         <div className="gpt3__brand section__padding">
             <div>
-                <img src={atlassian} alt="atlassian" />
+                <img src={google} alt="google" />
+            </div>
+            <div>
+                <img src={slack} alt="slack" />
             </div>
             <div>
                 <img src={dropbox} alt="dropbox" />
             </div>
             <div>
-                <img src={google} alt="google" />
-            </div>
-            <div>
                 <img src={shopify} alt="shopify" />
             </div>
             <div>
-                <img src={slack} alt="slack" />
+                <img src={atlassian} alt="atlassian" />
             </div>
         </div>
     );
