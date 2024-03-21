36c686a83b05b828f017defe04d16121ef89d940
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-20 23:43:26 -0500

    feat: :lipstick: add styles to feature component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -5,10 +5,11 @@ const Feature = ({ title, text }) => {
     return (
         <div className="gpt3__features-container__feature">
             <div className="gpt3__features-container__feature-title">
+              <div />
               <h1>{title}</h1>  
             </div>
             <div className='gpt3__features-container_feature-text'>
-                {text}
+                <p>{text}</p>
             </div>
         </div>
     );
