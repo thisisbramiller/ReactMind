d5e100f71113c33eee1994532222647f45798651
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-24 22:36:44 -0500

    fix: :bug: rename href link in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Menu = () => {
     return (
         <>
         <p><a href="#home">Home</a></p>
-        <p><a href="#wgpt3">What is GPT3?</a></p>
+        <p><a href="#gpt3desc">What is GPT3?</a></p>
         <p><a href="#possibility">Open AI</a></p>
         <p><a href="#features">Case Studies</a></p>
         <p><a href="#blog">Library</a></p>
