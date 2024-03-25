6bcbe8d7cc4ac4e64e4586834fa90c981fad6d4b
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-24 20:45:02 -0500

    feat: :lipstick: add styles to gpt3Description component

--- src/containers/gptThreeDescription/GPTThreeDescription.jsx
+++ src/containers/gptThreeDescription/GPTThreeDescription.jsx
@@ -6,7 +6,7 @@ const GPTThreeDescription = () => {
     return (
         <div className="gpt3__gpt3Description section__margin" id="gpt3desc">
             <div className="gpt3__gpt3Description-feature">
-                <Feature title="What is GPT-3" text="Carrot cake tart sweet gingerbread dessert gingerbread muffin macaroon. Candy cookie cheesecake jelly powder shortbread. Biscuit topping danish bear claw biscuit danish. Muffin lemon drops icing chocolate bar croissant halvah. Soufflé macaroon apple pie pudding macaroon dessert tiramisu toffee tiramisu. Tart chocolate cake danish biscuit jelly-o wafer jelly beans marzipan cotton candy. Cotton candy candy canes dessert toffee dessert croissant pie wafer marshmallow. Cotton candy shortbread danish ice cream sesame snaps gummi bears cookie chocolate chocolate. Cupcake tiramisu donut toffee pie ice cream. Chocolate cake chocolate bar lollipop jujubes lollipop apple pie carrot cake lollipop candy." />
+                <Feature title="What is GPT-3" text="Carrot cake tart sweet gingerbread dessert gingerbread muffin macaroon. Candy cookie cheesecake jelly powder shortbread. Biscuit topping danish bear claw biscuit danish. Muffin lemon drops icing chocolate bar croissant halvah. Soufflé macaroon apple pie pudding macaroon dessert tiramisu toffee tiramisu. ." />
             </div>
             <div className="gpt3__gpt3Description-heading">
                 <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
