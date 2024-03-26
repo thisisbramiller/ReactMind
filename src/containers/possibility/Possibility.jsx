7e9095066266f6788d94d24936bcb98655149168
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-25 21:05:58 -0500

    feat: :lipstick: add possibility image

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './possibility.css'
+import possibilityImage from '../../assets/possibility.png'
 
 const Possibility = () => {
     return (
-        <div>
-        <h1>Possibility</h1>
+        <div className="gpt3__possibility section_padding" id="possibility">
+            <div className="gpt3__possibility-image">
+                <img src={possibilityImage} alt="possibility" />
+            </div>
         </div>
     );
 }
